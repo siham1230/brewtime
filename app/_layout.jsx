fcf81a91f666ef4822ffa09bbd286d913276997a
siham1230 <ouchrifsiham86@gmail.com>
2025-10-24 11:19:10 +0100

    modification en layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack >
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ title: "Accueil" }} />
       <Stack.Screen name="MenuScreen" options={{ title: "Menu" }} />
     </Stack>
