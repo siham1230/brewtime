0885517d2a80a56e2b70a04d0bb6f2f95ee5cea1
faskadev <faskadropify@gmail.com>
2025-10-24 09:44:42 +0100

    newpush

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack >
       <Stack.Screen name="index" options={{ title: "Accueil" }} />
       <Stack.Screen name="MenuScreen" options={{ title: "Menu" }} />
     </Stack>
